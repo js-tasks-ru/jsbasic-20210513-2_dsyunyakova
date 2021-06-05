f8a02d1e9e9a124dae5203edf0a5f3fe3ac96daa
Di-gitcode <git.fr.end@gmail.com>
2021-06-05 09:08:35 +0300

    + 4-1

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  let str = '';
+  for (let user of users) {
+    
+    if (user.age <= age) {
+      str += `${user.name}, ${user.balance}\n`;
+    }
+  }
+  return str.trim();
 }
