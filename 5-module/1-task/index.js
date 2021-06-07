f2a01a549ad86352b72e1b106c74bc84fde46a90
Di-gitcode <git.fr.end@gmail.com>
2021-06-07 18:22:03 +0300

    задачи из модулей 3-5

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,8 @@
 function hideSelf() {
-  // ваш код...
+  let button = document.querySelector('.hide-self-button');
+  button.onclick = function() {
+    button.setAttribute('hidden', '');
+    console.log(button);
+  }
+  return button;
 }
