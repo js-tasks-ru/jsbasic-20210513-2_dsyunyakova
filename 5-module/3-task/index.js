fb26f93cd259e73ed08e0b004462c4e95166de9d
Di-gitcode <git.fr.end@gmail.com>
2021-06-12 16:51:42 +0300

    пролог

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  const inner = document.querySelector('.carousel__inner');
+  const rightArrow = document.querySelector('.carousel__arrow_right');
+  const leftArrow = document.querySelector('.carousel__arrow_left');
+  let counter = 0;
+  
+  rightArrow.addEventListener('click', () => {
+    counter -= 988;
+    inner.style.transform = `translateX(${counter}px)`;
+    (() => {
+      if (counter === -2964) {
+        rightArrow.style.display = 'none';
+      } else {
+        rightArrow.style.display = '';
+        console.log('no');
+      }
+    })();
+  });
+  
+  leftArrow.addEventListener('click', () => {
+    counter += 988;
+    inner.style.transform = `translateX(${counter}px)`;
+    if (counter === 0) {
+      leftArrow.style.display = 'none';
+    } else {
+      leftArrow.style.display = '';
+    }
+    
+  });
+  
+  return;
 }
+initCarousel();
