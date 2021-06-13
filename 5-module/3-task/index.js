4262f1dc8a630b14b3e76e0b798c28d9032bb946
Di-gitcode <git.fr.end@gmail.com>
2021-06-13 14:39:51 +0300

    пролог и модуль 6, задача 2

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -2,30 +2,33 @@ function initCarousel() {
   const inner = document.querySelector('.carousel__inner');
   const rightArrow = document.querySelector('.carousel__arrow_right');
   const leftArrow = document.querySelector('.carousel__arrow_left');
+  const width = inner.offsetWidth; 
   let counter = 0;
   
+  leftArrow.style.display = 'none';
+  
   rightArrow.addEventListener('click', () => {
-    counter -= 988;
-    inner.style.transform = `translateX(${counter}px)`;
-    (() => {
-      if (counter === -2964) {
-        rightArrow.style.display = 'none';
-      } else {
-        rightArrow.style.display = '';
-        console.log('no');
-      }
-    })();
+    counter++;
+    inner.style.transform = `translateX(${-counter * width}px)`;
+    if (counter === 3) {
+      rightArrow.style.display = 'none';
+      leftArrow.style.display = '';
+    } else {
+      rightArrow.style.display = '';
+      leftArrow.style.display = '';
+    }
   });
   
   leftArrow.addEventListener('click', () => {
-    counter += 988;
-    inner.style.transform = `translateX(${counter}px)`;
+    counter--;
+    inner.style.transform = `translateX(${-counter * width}px)`;
     if (counter === 0) {
       leftArrow.style.display = 'none';
+      rightArrow.style.display = '';
     } else {
       leftArrow.style.display = '';
+      rightArrow.style.display = '';
     }
-    
   });
   
   return;
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,6 +1,34 @@
 import createElement from '../../assets/lib/create-element.js';
 
+let x = {
+  name: "Laab kai chicken salad", // название товара
+  price: 10, // цена товара
+  category: "salads", // категория, к которой он относится, нам это понадобится чуть позже
+  image: "laab_kai_chicken_salad.png", // название картинки товара
+  id: "laab-kai-chicken-salad" // уникальный идентификатор товара, нужен для добавления товара в корзину
+}
+
 export default class ProductCard {
   constructor(product) {
+    this.name = product.name;
+    this.price = product.price;
+    this.image = product.image;
+    this.id = product.id;
+    
+    let div = document.createElement('div');
+    div.classList.add('card');
+    div.insertAdjacentHTML('afterbegin', `<div class="card__top">
+        <img src="/assets/images/products/${this.image}" class="card__image" alt="product">
+        <span class="card__price">€${this.price.toFixed(2)}</span>
+    </div>
+    <div class="card__body">
+        <div class="card__title">${this.name}</div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>`);
+    
+    this.elem = document.querySelector('.card');
   }
+  
 }
