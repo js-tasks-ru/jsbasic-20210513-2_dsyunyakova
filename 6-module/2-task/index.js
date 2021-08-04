bf44206b46d3bf97a3a912e6760f3ed7fa45c9f2
Di-gitcode <git.fr.end@gmail.com>
2021-08-04 19:26:28 +0300

    5.3 и частично модуль 6

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -8,9 +8,23 @@ export default class ProductCard {
     this.id = product.id;
     let div = document.createElement('div');
     div.classList.add('card');
-    this.elem = div;
+    
+    
+    const card = createElement(`
+    <div class="card__top">
+        <img src="/assets/images/products/${this.image}" class="card__image" alt="product">
+        <span class="card__price">€${this.price.toFixed(2)}</span>
+    </div>
+    <div class="card__body">
+        <div class="card__title">${this.name}</div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>
+    `);
+    this.elem = card;
 
-    div.insertAdjacentHTML('afterbegin', `<div class="card__top">
+    /*div.insertAdjacentHTML('afterbegin', `<div class="card__top">
         <img src="/assets/images/products/${this.image}" class="card__image" alt="product">
         <span class="card__price">€${this.price.toFixed(2)}</span>
     </div>
@@ -20,6 +34,9 @@ export default class ProductCard {
             <img src="/assets/images/icons/plus-icon.svg" alt="icon">
         </button>
     </div>`);
+    */
+   
+    
   }
 }
 
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -16,85 +16,92 @@ export default class Carousel {
       this.id = this.slides[i].id;
     }
     
-    let carousel = document.createElement('div');
-    carousel.classList.add('carousel');
-    carousel.insertAdjacentHTML('afterbegin', `<div class="carousel__arrow carousel__arrow_right">
-    <img src="/assets/images/icons/angle-icon.svg" alt="icon">
-    </div>
-    <div class="carousel__arrow carousel__arrow_left">
-      <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
-    </div>`);
-    this.elem = carousel;
+    let carouselArrows = `
+      <div class="carousel__arrow carousel__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </div>
+      <div class="carousel__arrow carousel__arrow_left">
+        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+      </div>
+    `;
     
-    let carouselInner = document.createElement('div');
-    carouselInner.classList.add('carousel__inner');
-    carouselInner.insertAdjacentHTML('afterbegin', `<div class="carousel__slide" data-id="penang-shrimp">
-    <img src="/assets/images/carousel/${this.image}" class="carousel__img" alt="slide">
-    <div class="carousel__caption">
-      <span class="carousel__price">€${this.price.toFixed(2)}</span>
-      <div class="carousel__title">${this.name}</div>
-      <button type="button" class="carousel__button">
-        <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-      </button>
-    </div>
-  </div>`);
+    let carouselCard = `
+      <div class="carousel__slide" data-id="penang-shrimp">
+          <img src="/assets/images/carousel/${this.image}" class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+          <span class="carousel__price">€${this.price.toFixed(2)}</span>
+          <div class="carousel__title">${this.name}</div>
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `;
+    let carouselRoot = createElement(`
+      <div class="container">
+        <div class="carousel">
+          ${carouselArrows}
+        </div>
+        <div class="carousel__inner">
+          ${carouselCard}
+        </div>
+      </div>
+    `);
+    
+    this.elem = carouselRoot;
+    const width = carouselCard.offsetWidth;
   
-  document.body.append(this.elem);
-  this.elem.appendChild(carouselInner);
-  const width = carouselInner.offsetWidth;
-  
-  let slideImage = () => {
-    const rightArrow = document.querySelector('.carousel__arrow_right');
-    const leftArrow = document.querySelector('.carousel__arrow_left');
-     
-    let counter = 0;
-        
-    leftArrow.style.display = 'none';
-    rightArrow.style.display = '';
-    
-    rightArrow.addEventListener('click', () => {
-      counter++;
-      carouselInner.style.transform = `translateX(${-counter * width}px)`;
-      if (counter === slidesLength) {
-        rightArrow.style.display = 'none';
-        leftArrow.style.display = '';
-      } else {
-        rightArrow.style.display = '';
-        leftArrow.style.display = '';
-        
-      }
-    });
-    
-    leftArrow.addEventListener('click', () => {
-      counter--;
-      carouselInner.style.transform = `translateX(${-counter * width}px)`;
-      if (counter === 0) {
-        leftArrow.style.display = 'none';
-        rightArrow.style.display = '';
-      } else {
-        leftArrow.style.display = '';
-        rightArrow.style.display = '';
-      }
-    })
-    
-    return;
-  }
-  slideImage();
-  
-  let addToCart = () => {
-    let plusButton = document.querySelector('.carousel__button');
-    
-    plusButton.addEventListener('click', () => {
-      new CustomEvent("product-add", {
-        detail: this.id,
-        bubbles: true
+    let slideImage = () => {
+      const rightArrow = document.querySelector('.carousel__arrow_right');
+      const leftArrow = document.querySelector('.carousel__arrow_left');
+      
+      let counter = 0;
+          
+      leftArrow.style.display = 'none';
+      rightArrow.style.display = '';
+      
+      rightArrow.addEventListener('click', () => {
+        counter++;
+        carouselCard.style.transform = `translateX(${-counter * width}px)`;
+        if (counter === slidesLength) {
+          rightArrow.style.display = 'none';
+          leftArrow.style.display = '';
+        } else {
+          rightArrow.style.display = '';
+          leftArrow.style.display = '';
+          
+        }
       });
       
-    });
-    return;
-  }
+      leftArrow.addEventListener('click', () => {
+        counter--;
+        carouselCard.style.transform = `translateX(${-counter * width}px)`;
+        if (counter === 0) {
+          leftArrow.style.display = 'none';
+          rightArrow.style.display = '';
+        } else {
+          leftArrow.style.display = '';
+          rightArrow.style.display = '';
+        }
+      })
+      
+      return;
+    }
+    slideImage();
   
-  addToCart();
+    let addToCart = () => {
+      let plusButton = document.querySelector('.carousel__button');
+      
+      plusButton.addEventListener('click', () => {
+        new CustomEvent("product-add", {
+          detail: this.id,
+          bubbles: true
+        });
+        
+      });
+      return;
+    }
+    addToCart();
   
   }
 }
